74ac171f8f4aa895e2291aacac1dc8938509c9d5
conormmd <cnr.dvr@gmail.com>
2021-10-05 22:12:43 +0100

    Finished problem 35
    got 55 circular primes under 1million

--- problem35.py
+++ problem35.py
@@ -3,32 +3,36 @@ import numpy as np
 i = 1
 n = 0
 
-primes = [2]
-
-def primeTest(test, primes):
-    n = len(primes)
-    num_factors = 0
-    for i in range(n):
-        test_type = test/primes[i]
-        if float.is_integer(test_type) == True:
-            num_factors = num_factors+1
-        else:
-            continue
-    if num_factors == 0:
-        return(test)
-    else:
-        return
+circ_primes = []
 
 
-while n<100:
-    i = i+1
-    j = 2*i-1
-    test = primeTest(j,primes)
-    if type(test) == int:
-        primes.append(test)
-    n_primes = len(primes)
+def isPrime(test):
+    n = int(np.sqrt(test))
+    for i in range(2,n+1):
+        if test % i == 0:
+            return False
+    return True
+
+def circTest(test):
+    str_test = str(test)
+    n = len(str_test)
+    i=0
+    while i<n:
+        i = i+1
+        str_test = str_test[-1] + str_test[0:-1]
+        if isPrime(int(str_test)) == False:
+            return False
+    return True
+
+print(isPrime(13))
+print(circTest(3001))
+
+"""for i in range(2,1000000):
+    if circTest(i) == True:
+        circ_primes.append(i)
+
+print(len(circ_primes))"""
 
 
 
-print(primes)
 
